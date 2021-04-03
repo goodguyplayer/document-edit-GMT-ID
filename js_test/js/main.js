8678a18027040f2ee4a05a710210572746f883bc
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-04-03 15:12:53 -0300

    main.js - updated onRun
    Iterating per cell in the table

--- js_test/js/main.js
+++ js_test/js/main.js
@@ -9,5 +9,15 @@ document.getElementById("run").onclick = function() {onRun()};
 
 
 function onRun(){
-    
+    var table = document.getElementById("informationandrequirements");
+    for (var i = 1, row; row = table.rows[i]; i++) {
+        console.log(row.cells[cellEnum.ID]);
+
+        if (row.cells[cellEnum.ARTIFACT_TYPE].innerText === "Requirement"){
+            row.cells[1].innerHTML = "Test";
+            
+        }
+
+     }
 }
+
